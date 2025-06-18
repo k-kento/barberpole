52ac38b83b6a65fdbfc60468982912c2115b3a84
k-kento <k.kento.918.124@gmail.com>
2025-06-18 21:03:25 +0900

    app module に audio の依存を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.datastore)
     implementation(libs.aboutlibraries.core)
     implementation(libs.aboutlibraries.compose)
+    implementation(project(":audio"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
