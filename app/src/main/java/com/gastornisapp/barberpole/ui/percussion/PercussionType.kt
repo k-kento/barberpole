26e3942f98ac172cb1807cb4424f2024b0cb9839
k-kento <k.kento.918.124@gmail.com>
2025-07-20 23:24:55 +0900

    パーカッションアイコンを追加

--- app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionType.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionType.kt
@@ -5,10 +5,10 @@ import com.gastornisapp.barberpole.R
 import com.gastornisapp.soundlib.AudioResource
 
 enum class PercussionType(@DrawableRes val drawableRes: Int, val color: Int) {
-    Bongo(drawableRes = R.drawable.conga, 0xF44336),
+    Bongo(drawableRes = R.drawable.bongo, 0xF44336),
     Pan(drawableRes = R.drawable.pan, 0xFF9800),
-    Shaker(drawableRes = R.drawable.conga, 0xFFEB3B),
-    Taiko(drawableRes = R.drawable.conga, 0x4CAF50),
+    Shaker(drawableRes = R.drawable.shaker, 0xFFEB3B),
+    Taiko(drawableRes = R.drawable.taiko, 0x4CAF50),
     Tambourines(drawableRes = R.drawable.tamburins, 0x2196F3),
     Stick(drawableRes = R.drawable.stick, 0x3F51B5),
     Bell(drawableRes = R.drawable.bell, 0x9C27B0),
