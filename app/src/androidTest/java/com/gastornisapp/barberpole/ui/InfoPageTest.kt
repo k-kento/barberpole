bc1ee03a22d8bfa4b3b189dcfc22a5447fbd3e37
k-kento <k.kento.918.124@gmail.com>
2025-08-03 18:44:01 +0900

    InfoPageTest.kt 修正

--- app/src/androidTest/java/com/gastornisapp/barberpole/ui/InfoPageTest.kt
+++ app/src/androidTest/java/com/gastornisapp/barberpole/ui/InfoPageTest.kt
@@ -1,6 +1,5 @@
 package com.gastornisapp.barberpole.ui
 
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -9,14 +8,24 @@ import androidx.compose.ui.test.performClick
 import androidx.navigation.compose.ComposeNavigator
 import androidx.navigation.testing.TestNavHostController
 import com.gastornisapp.barberpole.HiltComponentActivity
-import com.gastornisapp.barberpole.ui.info.InfoPage
-import com.gastornisapp.barberpole.ui.info.InfoViewModel
+import com.gastornisapp.barberpole.di.RepositoryModule
+import com.gastornisapp.barberpole.domain.model.SemVer
+import com.gastornisapp.barberpole.domain.repository.AppSettingsRepository
+import dagger.Module
+import dagger.Provides
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import javax.inject.Singleton
 
+@UninstallModules(RepositoryModule::class)
 @HiltAndroidTest
 class InfoPageTest {
 
@@ -31,39 +40,17 @@ class InfoPageTest {
     @Before
     fun setup() {
         hiltRule.inject()
-    }
 
-    @Composable
-    private fun initNavController(): TestNavHostController {
-        val controller = TestNavHostController(LocalContext.current).apply {
-            navigatorProvider.addNavigator(ComposeNavigator())
-        }
-        navController = controller
-        return controller
-    }
-
-    private fun launchWithInfoPage(viewModel: InfoViewModel? = null) {
         composeTestRule.setContent {
-            val nav = initNavController()
-            InfoPage(nav, viewModel ?: defaultViewModel())
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            AppNavGraph(startDestination = PageType.Info.route, navController = navController)
         }
     }
-
-    private fun launchWithAppNavGraph() {
-        composeTestRule.setContent {
-            val nav = initNavController()
-            AppNavGraph(
-                startDestination = PageType.Info.route,
-                navController = nav
-            )
-        }
-    }
-
-    private fun defaultViewModel() = InfoViewModel(FakeAppSettingsRepository())
 
     @Test
     fun infoPage_displaysAllListItems() {
-        launchWithInfoPage()
         composeTestRule.onNodeWithText("利用規約").assertIsDisplayed()
         composeTestRule.onNodeWithText("プライバシーポリシー").assertIsDisplayed()
         composeTestRule.onNodeWithText("OSS ライセンス").assertIsDisplayed()
@@ -73,29 +60,35 @@ class InfoPageTest {
 
     @Test
     fun clickingTerms_navigatesToTermsPage() {
-        launchWithAppNavGraph()
         composeTestRule.onNodeWithText("利用規約").performClick()
         assert(navController.currentBackStackEntry?.destination?.route?.startsWith("webpage/") == true)
     }
 
     @Test
     fun clickingPrivacy_navigatesToPrivacyPage() {
-        launchWithAppNavGraph()
         composeTestRule.onNodeWithText("プライバシーポリシー").performClick()
         assert(navController.currentBackStackEntry?.destination?.route?.startsWith("webpage/") == true)
     }
 
     @Test
     fun clickingOssLicense_navigatesToLicensePage() {
-        launchWithAppNavGraph()
         composeTestRule.onNodeWithText("OSS ライセンス").performClick()
         navController.assertCurrentRoute(PageType.License.route)
     }
 }
 
-private class FakeAppSettingsRepository : BaseFakeAppSettingsRepository() {
-    override fun getVersionName(): String {
-        return "1.2.3"
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [RepositoryModule::class]
+)
+object FakeRepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideAppSettingsRepository(): AppSettingsRepository {
+        val mock = mockk<AppSettingsRepository>()
+        every { mock.getCurrentAppVersion() } returns Result.success(SemVer.parse("1.2.3"))
+        return mock
     }
 }
-
