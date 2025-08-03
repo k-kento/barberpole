a7863d99dc095a72e50cb7291a02b38b65e3821d
k-kento <k.kento.918.124@gmail.com>
2025-08-03 21:54:03 +0900

    Notice Model を追加

--- app/src/main/java/com/gastornisapp/barberpole/domain/model/Notice.kt
+++ app/src/main/java/com/gastornisapp/barberpole/domain/model/Notice.kt
@@ -1,4 +1,16 @@
 package com.gastornisapp.barberpole.domain.model
 
-class Notice {
-}
+class Notice(
+    val id: String,
+    val title: String,
+    val message: String,
+    val url: String? = null,
+    /**
+     * 公開開始時(秒)
+     */
+    val startAt: Long,
+    /**
+     * 公開終了時(秒)
+     */
+    val endAt: Long
+)
