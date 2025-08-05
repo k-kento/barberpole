8d0211763e3d1caa543f30c3cd38c43e62b41c30
k-kento <k.kento.918.124@gmail.com>
2025-08-05 17:44:11 +0900

    起動処理をリファクタリング

--- app/src/main/java/com/gastornisapp/barberpole/MainActivity.kt
+++ app/src/main/java/com/gastornisapp/barberpole/MainActivity.kt
@@ -4,30 +4,41 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.gastornisapp.barberpole.ui.AppNavGraph
-import com.gastornisapp.barberpole.ui.MainViewModel
+import com.gastornisapp.barberpole.ui.LoadingPage
+import com.gastornisapp.barberpole.ui.AppStartupViewModel
+import com.gastornisapp.barberpole.ui.AppStartupUiStatus
 import com.gastornisapp.barberpole.ui.theme.BarberPoleTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
+    private val viewModel: AppStartupViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        enableEdgeToEdge()
+
         val splashScreen = installSplashScreen()
+        splashScreen.setKeepOnScreenCondition { viewModel.uiStatus.value == AppStartupUiStatus.ShowingSplash }
+
+        viewModel.initialize()
 
         setContent {
-            val viewModel: MainViewModel = hiltViewModel()
-            splashScreen.setKeepOnScreenCondition { viewModel.isSplashScreenShowing.value }
-
             BarberPoleTheme {
-                AppNavGraph(viewModel)
+                val status by viewModel.uiStatus.collectAsState()
+                when (val status = status) {
+                    AppStartupUiStatus.ShowingSplash -> Unit
+                    AppStartupUiStatus.ShowingProgress -> LoadingPage()
+                    is AppStartupUiStatus.Completed -> AppNavGraph(startDestination = status.startDestination.route)
+                }
             }
         }
-
-        enableEdgeToEdge()
     }
 }
