e453933a88fc11e5738c2c8df367e819f60af571
k-kento <k.kento.918.124@gmail.com>
2025-08-08 15:08:10 +0900

    SwarmPage.kt を追加

---
+++ app/src/main/java/com/gastornisapp/barberpole/ui/swarm/SwarmPage.kt
@@ -0,0 +1,23 @@
+package com.gastornisapp.barberpole.ui.swarm
+
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun SwarmPage() {
+    Scaffold { paddingValues ->
+
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize(),
+            factory = {
+                SwarmView(context = it)
+            },
+        )
+    }
+}
