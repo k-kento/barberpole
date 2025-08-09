578b37b25c43560a4a4e0ffba9e20621fac752ee
k-kento <k.kento.918.124@gmail.com>
2025-08-10 00:17:07 +0900

    PercussionPage.kt
    IconButton を OutlinedIconButton に変更

--- app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionPage.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionPage.kt
@@ -1,6 +1,6 @@
 package com.gastornisapp.barberpole.ui.percussion
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,13 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
@@ -90,15 +90,17 @@ fun ButtonsRow(
         horizontalArrangement = Arrangement.spacedBy(4.dp)
     ) {
         for (type in types) {
-            IconButton(
+            // 円形 かつ RippleEffect を適用するため OutlinedIconButton を使用する
+            OutlinedIconButton(
                 onClick = {
                     onClick(type)
                 },
+                border= BorderStroke(0.dp, Color(0X00000000)),
+                shape = CircleShape,
+                colors = IconButtonDefaults.outlinedIconButtonColors(Color(type.color or 0xFF000000.toInt())),
                 modifier = Modifier
                     .weight(1f)
                     .aspectRatio(1f)
-                    .clip(CircleShape)
-                    .background(Color(type.color or 0xFF000000.toInt()))
             ) {
                 Icon(
                     modifier = Modifier.padding(12.dp),
