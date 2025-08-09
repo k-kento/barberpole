3237458d0b14a68d615800a3ece5e740d98f958c
k-kento <k.kento.918.124@gmail.com>
2025-08-09 22:56:53 +0900

    PercussionPage.kt
    PercussionView を削除し、 Compose で書き直し

--- app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionPage.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/percussion/PercussionPage.kt
@@ -1,14 +1,27 @@
 package com.gastornisapp.barberpole.ui.percussion
 
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
 import com.gastornisapp.soundlib.AudioLib
 import com.gastornisapp.soundlib.AudioResource
 
@@ -36,17 +49,64 @@ fun PercussionPage() {
             }
         }
 
-        AndroidView(
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-            factory = {
-                PercussionView(context = it).apply {
-                    onClicked = { type ->
-                        audioLibs[type.toAudioResource()]?.start()
-                    }
-                }
-            },
-        )
+                .padding(paddingValues)
+                .padding(horizontal = 16.dp)
+        ) {
+            GridButtons(onClick = { type ->
+                audioLibs[type.toAudioResource()]?.start()
+            })
+        }
+    }
+}
+
+@Composable
+fun GridButtons(onClick: (PercussionType) -> Unit) {
+    val types = PercussionType.entries
+    val half = (types.size + 1) / 2
+
+    Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+        ButtonsRow(
+            types = types.take(half),
+            onClick = {
+                onClick(it)
+            })
+        ButtonsRow(
+            types = types.drop(half),
+            onClick = {
+                onClick(it)
+            })
+    }
+}
+
+@Composable
+fun ButtonsRow(
+    types: List<PercussionType>,
+    onClick: (PercussionType) -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        for (type in types) {
+            IconButton(
+                onClick = {
+                    onClick(type)
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .aspectRatio(1f)
+                    .clip(CircleShape)
+                    .background(Color(type.color or 0xFF000000.toInt()))
+            ) {
+                Icon(
+                    modifier = Modifier.padding(12.dp),
+                    imageVector = ImageVector.vectorResource(id = type.drawableRes),
+                    tint = Color.White,
+                    contentDescription = "",
+                )
+            }
+        }
     }
 }
