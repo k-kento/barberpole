46267ee71b6362fde72f5746419f60fbb73b2d5b
k-kento <k.kento.918.124@gmail.com>
2025-08-10 23:46:22 +0900

    PercussionPage を Compose で書き換え
    - OpenGL で描画していた打楽器一覧画面を Compose で書き換えた
    - 音声再生処理を SoundPool を使ったものに置き換え
    - audio モジュールを削除

---
+++ app/src/main/java/com/gastornisapp/barberpole/usecase/PlaySoundUseCase.kt
@@ -0,0 +1,20 @@
+package com.gastornisapp.barberpole.usecase
+
+import com.gastornisapp.barberpole.domain.port.SoundPlayer
+import javax.inject.Inject
+
+class PlaySoundUseCase @Inject constructor(private val soundPlayer: SoundPlayer) {
+
+    suspend fun load(fileName: String) {
+        soundPlayer.load(fileName)
+    }
+
+    fun play(fileName: String) {
+        soundPlayer.play(fileName)
+    }
+
+    fun release() {
+        soundPlayer.release()
+    }
+
+}
--- audio/src/main/java/com/gastornisapp/soundlib/AudioResource.kt
+++ audio/src/main/java/com/gastornisapp/soundlib/AudioResource.kt
@@ -1,12 +1,12 @@
 package com.gastornisapp.soundlib
 
 enum class AudioResource(internal val fileName: String) {
-    Bongo("bongo.wav"),
-    Pan("pan.wav"),
-    Shaker("shaker.wav"),
-    Taiko("taiko.wav"),
-    Tambourines("tambourines.wav"),
-    Stick("stick.wav"),
-    Bell("bell.wav"),
-    Conga("conga.wav"),
+    Bongo("audio/bongo.wav"),
+    Pan("audio/pan.wav"),
+    Shaker("audio/shaker.wav"),
+    Taiko("audio/taiko.wav"),
+    Tambourines("audio/tambourines.wav"),
+    Stick("audio/stick.wav"),
+    Bell("audio/bell.wav"),
+    Conga("audio/conga.wav"),
 }
