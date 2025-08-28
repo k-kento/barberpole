dad5d3209bcd74e4071a2d7637f384e1bf562fb1
k-kento <k.kento.918.124@gmail.com>
2025-08-28 23:35:29 +0900

    色指定ができるよう FishShaderProgram.kt を修正

--- app/src/main/java/com/gastornisapp/barberpole/ui/swarm/FishShaderProgram.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/swarm/FishShaderProgram.kt
@@ -10,13 +10,19 @@ class FishShaderProgram : ShaderProgram(
 
     var posHandle = -1
     private var mvpMatrixHandle = -1
+    private var colorHandle = -1
 
     override fun onInitialized(program: Int) {
         posHandle = GLES30.glGetAttribLocation(program, "aPosition")
         mvpMatrixHandle = GLES30.glGetUniformLocation(program, "uMvpMatrix")
+        colorHandle = GLES30.glGetUniformLocation(program, "uColor")
     }
 
     fun setMvpMatrix(mvpMatrix: FloatArray) {
         GLES30.glUniformMatrix4fv(mvpMatrixHandle, 1, false, mvpMatrix, 0)
+    }
+
+    fun setColor(color: FloatArray) {
+        GLES30.glUniform4fv(colorHandle, 1, color, 0)
     }
 }
