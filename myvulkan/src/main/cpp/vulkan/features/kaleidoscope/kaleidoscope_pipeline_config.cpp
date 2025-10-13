6488892c2c13d7f2553f45ce35141ff2b82bf018
k-kento <k.kento.918.124@gmail.com>
2025-10-13 14:17:51 +0900

    pipeline builder を追加

---
+++ myvulkan/src/main/cpp/vulkan/features/kaleidoscope/kaleidoscope_pipeline_config.cpp
@@ -0,0 +1,102 @@
+
+#include "kaleidoscope_pipeline_config.hpp"
+#include "pipeline_builder.hpp"
+#include "vulkan_utils.h"
+#include "vertex.hpp"
+#include "instance_data.hpp"
+
+vk::UniquePipeline
+KaleidoscopePipelineConfig::createPipeline(VulkanContext &context,
+                                           vk::PipelineLayout &pipelineLayout,
+                                           RenderPass &renderPass) {
+
+    vk::Device device = context.getVkDevice();
+    AAssetManager *assetManager = context.getAssetManager();
+
+    std::vector<char> vertexShaderSrc = VulkanUtils::readFile(assetManager, "shaders/triangle.vert.spv");
+    std::vector<char> fragmentShaderSrc = VulkanUtils::readFile(assetManager, "shaders/triangle.frag.spv");
+
+    auto vertexShaderModule = VulkanUtils::createShaderModule(device, vertexShaderSrc);
+    auto fragmentShaderModule = VulkanUtils::createShaderModule(device, fragmentShaderSrc);
+
+    auto shaderStages = createShaderStages(vertexShaderModule.get(), fragmentShaderModule.get());
+    auto vertexInputInfo = createVertexConfig();
+    auto builder = PipelineBuilder(shaderStages, vertexInputInfo, renderPass.getVkRenderPass(), pipelineLayout);
+
+    builder.rasterizer = vk::PipelineRasterizationStateCreateInfo{}
+            .setDepthClampEnable(VK_FALSE)
+            .setRasterizerDiscardEnable(VK_FALSE)
+            .setPolygonMode(vk::PolygonMode::eFill)
+            .setCullMode(vk::CullModeFlagBits::eNone)
+            .setDepthBiasEnable(VK_FALSE)
+            .setLineWidth(1.0f);
+
+    return builder.build(context.getVkDevice(), nullptr);
+}
+
+vk::PipelineVertexInputStateCreateInfo KaleidoscopePipelineConfig::createVertexConfig() {
+    // 頂点バッファ
+    vk::VertexInputBindingDescription vertexBinding{};
+    vertexBinding.binding = 0;
+    vertexBinding.stride = sizeof(Vertex);
+    vertexBinding.inputRate = vk::VertexInputRate::eVertex;
+
+    // インスタンスバッファ
+    vk::VertexInputBindingDescription instanceBinding{};
+    instanceBinding.binding = 1;
+    instanceBinding.stride = sizeof(InstanceData);
+    instanceBinding.inputRate = vk::VertexInputRate::eInstance;
+
+    std::vector<vk::VertexInputBindingDescription> bindings = {vertexBinding, instanceBinding};
+
+    // 位置属性
+    vk::VertexInputAttributeDescription posAttr{};
+    posAttr.location = 0;
+    posAttr.binding = 0;
+    posAttr.format = vk::Format::eR32G32Sfloat;
+    posAttr.offset = offsetof(Vertex, position);
+
+    // 色属性
+    vk::VertexInputAttributeDescription colorAttr{};
+    colorAttr.location = 5;                        // location=1 にする
+    colorAttr.binding = 0;                          // 同じバインディング（頂点バッファ0）
+    colorAttr.format = vk::Format::eR32G32B32Sfloat; // vec3
+    colorAttr.offset = offsetof(Vertex, color);    // Vertex 内のオフセット
+
+    std::vector<vk::VertexInputAttributeDescription> attributes;
+
+    attributes.push_back(posAttr);
+    attributes.push_back(colorAttr);
+
+    for (uint32_t i = 0; i < 4; ++i) {
+        vk::VertexInputAttributeDescription instAttr{};
+        instAttr.location = 1 + i;
+        instAttr.binding = 1;
+        instAttr.format = vk::Format::eR32G32B32A32Sfloat;
+        instAttr.offset = offsetof(InstanceData, model) + sizeof(glm::vec4) * i;
+        attributes.push_back(instAttr);
+    }
+
+    vk::PipelineVertexInputStateCreateInfo vertexInput{};
+    vertexInput.vertexBindingDescriptionCount = static_cast<uint32_t>(bindings.size());
+    vertexInput.pVertexBindingDescriptions = bindings.data();
+    vertexInput.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributes.size());
+    vertexInput.pVertexAttributeDescriptions = attributes.data();
+
+    return vertexInput;
+}
+
+std::vector<vk::PipelineShaderStageCreateInfo>
+KaleidoscopePipelineConfig::createShaderStages(vk::ShaderModule &vertexModule, vk::ShaderModule &fragmentModule) {
+    vk::PipelineShaderStageCreateInfo vertStage{};
+    vertStage.stage = vk::ShaderStageFlagBits::eVertex;
+    vertStage.module = vertexModule;
+    vertStage.pName = "main";
+
+    vk::PipelineShaderStageCreateInfo fragStage{};
+    fragStage.stage = vk::ShaderStageFlagBits::eFragment;
+    fragStage.module = fragmentModule;
+    fragStage.pName = "main";
+
+    return {vertStage, fragStage};
+}
--- myvulkan/src/main/cpp/vulkan/features/kaleidoscope/kaleidoscope_renderer.cpp
+++ myvulkan/src/main/cpp/vulkan/features/kaleidoscope/kaleidoscope_renderer.cpp
@@ -1,7 +1,5 @@
 #include "kaleidoscope_renderer.h"
-#include "vulkan_utils.h"
 #include "view_bounds.hpp"
-#include "graphics_pipeline.h"
 #include "vulkan_buffer.h"
 #include "descriptor.hpp"
 #include "ubo_buffer.hpp"
@@ -9,9 +7,14 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "log.h"
 #include "vertex.hpp"
+#include "mirror_tile_grid.hpp"
+#include "kaleidoscope_pipeline_config.hpp"
 
-KaleidoscopeRenderer::KaleidoscopeRenderer(VulkanContext *vkContext, ANativeWindow *window) : VulkanRenderer(vkContext,
-                                                                                                             window) {
+KaleidoscopeRenderer::KaleidoscopeRenderer(VulkanContext *vkContext,
+                                           ANativeWindow *window,
+                                           RenderPass *renderPass)
+        : mVkContext(vkContext), mRenderPass(renderPass) {
+    auto device = mVkContext->getVkDevice();
 
     auto windowWidth = ANativeWindow_getWidth(window);
     auto windowHeight = ANativeWindow_getHeight(window);
@@ -21,32 +24,33 @@ KaleidoscopeRenderer::KaleidoscopeRenderer(VulkanContext *vkContext, ANativeWind
     auto viewBounds = ViewBounds::fromSize(windowWidth, windowHeight);
     mProjectionMatrix = viewBounds.toOrthoMatrix();
 
+    auto slices = MirrorTileGrid::createTileGrid(&viewBounds);
+
+    for (auto &slice: slices) {
+        instanceData.push_back({slice});
+    }
+
     createBuffer();
     createDescriptorSet();
     mUboWrapper.emplace(mUboBuffer->getMemory());
-    createGraphicsPipeline();
-    recordCommandBuffers();
+
+    vk::PipelineLayoutCreateInfo layoutInfo{};
+    mPipelineLayout = device.createPipelineLayoutUnique(layoutInfo);
+    mPipeline = KaleidoscopePipelineConfig::createPipeline(*mVkContext, mPipelineLayout.get(), *mRenderPass);
 }
 
 void KaleidoscopeRenderer::renderFrame() {
-    UBO ubo{};
-    ubo.projection = mProjectionMatrix;
-    mUboWrapper->update(mVkContext->getVkDevice(), ubo);
+    mUboData.projection = mProjectionMatrix;
+    mUboWrapper->update(mVkContext->getVkDevice(), mUboData);
 }
 
-void KaleidoscopeRenderer::recordDrawCommands(vk::CommandBuffer cmdBuffer, uint32_t imageIndex) {
+void KaleidoscopeRenderer::recordDrawCommands(vk::CommandBuffer cmdBuffer) {
     // パイプラインバインド
-    cmdBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, mGraphicsPipeline->getPipeline());
+    cmdBuffer.bindPipeline(vk::PipelineBindPoint::eGraphics, mPipeline.get());
 
     // ディスクリプタセットバインド
     std::array<vk::DescriptorSet, 1> sets = {mDescriptorSet};
-    cmdBuffer.bindDescriptorSets(
-            vk::PipelineBindPoint::eGraphics,
-            mGraphicsPipeline->getPipelineLayout(),
-            0,
-            sets,
-            nullptr
-    );
+    cmdBuffer.bindDescriptorSets(vk::PipelineBindPoint::eGraphics, mPipelineLayout.get(), 0, sets, nullptr);
 
     // 頂点バッファとインスタンスバッファ
     vk::Buffer vertexBuffers[] = {mVertexBuffer->getBuffer()};
@@ -60,9 +64,8 @@ void KaleidoscopeRenderer::recordDrawCommands(vk::CommandBuffer cmdBuffer, uint3
     cmdBuffer.bindIndexBuffer(mIndexBuffer->getBuffer(), 0, vk::IndexType::eUint16);
 
     auto indexCount = static_cast<uint32_t>(mRegularPolygonMesh->indices.size());
-    cmdBuffer.drawIndexed(indexCount, 10, 0, 0, 0);
-
-    LOGI("recordDrawCommands done for command buffer %d", imageIndex);
+    uint32_t instanceCount = instanceData.size();
+    cmdBuffer.drawIndexed(indexCount, instanceCount, 0, 0, 0);
 }
 
 void KaleidoscopeRenderer::createBuffer() {
@@ -95,6 +98,8 @@ void KaleidoscopeRenderer::createBuffer() {
 
     mIndexBuffer->copyFrom(mRegularPolygonMesh->indices.data(), bufferSize);
 
+    /* Instance Buffer */
+    uint32_t instanceCount = instanceData.size();
     VkDeviceSize instanceBufferSize = sizeof(InstanceData) * instanceCount;
 
     mInstanceBuffer = std::make_unique<VulkanBuffer>(
@@ -105,7 +110,7 @@ void KaleidoscopeRenderer::createBuffer() {
             vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
     );
 
-    mInstanceBuffer->copyFrom(instances.data(), instanceBufferSize);
+    mInstanceBuffer->copyFrom(instanceData.data(), instanceBufferSize);
 
     /* UBO Buffer */
     mUboBuffer = std::make_unique<VulkanBuffer>(
@@ -117,23 +122,6 @@ void KaleidoscopeRenderer::createBuffer() {
     );
 }
 
-void KaleidoscopeRenderer::createGraphicsPipeline() {
-    vk::Device device = mVkContext->getVkDevice();
-    AAssetManager *assetManager = mVkContext->getAssetManager();
-
-    std::vector<char> vertexShaderSrc = VulkanUtils::readFile(assetManager, "shaders/triangle.vert.spv");
-    std::vector<char> fragmentShaderSrc = VulkanUtils::readFile(assetManager, "shaders/triangle.frag.spv");
-
-    auto vertexShaderModule = VulkanUtils::createShaderModule(device, vertexShaderSrc);
-    auto fragmentShaderModule = VulkanUtils::createShaderModule(device, fragmentShaderSrc);
-
-    std::vector<vk::ShaderModule> shaders = {vertexShaderModule.get(), fragmentShaderModule.get()};
-    mGraphicsPipeline = std::make_unique<GraphicsPipeline>(device,
-                                                           createPipeLineConfig(),
-                                                           mRenderPass->getVkRenderPass(),
-                                                           shaders);
-}
-
 void KaleidoscopeRenderer::createDescriptorSet() {
     vk::Device device = mVkContext->getVkDevice();
 
@@ -143,53 +131,5 @@ void KaleidoscopeRenderer::createDescriptorSet() {
     config.maxSets = 1;
 
     mDescriptorManager = std::make_unique<Descriptor>(device, config);
-
     mDescriptorSet = mDescriptorManager->allocateDescriptorSet(device, mUboBuffer->getBuffer(), sizeof(UBO));
-}
-
-PipelineConfig KaleidoscopeRenderer::createPipeLineConfig() {
-    // 頂点バッファ
-    vk::VertexInputBindingDescription vertexBinding{};
-    vertexBinding.binding = 0;
-    vertexBinding.stride = sizeof(Vertex);
-    vertexBinding.inputRate = vk::VertexInputRate::eVertex;
-
-    // インスタンスバッファ
-    vk::VertexInputBindingDescription instanceBinding{};
-    instanceBinding.binding = 1;
-    instanceBinding.stride = sizeof(InstanceData);
-    instanceBinding.inputRate = vk::VertexInputRate::eInstance;
-
-    std::vector<vk::VertexInputBindingDescription> bindings = {vertexBinding, instanceBinding};
-
-    // 頂点属性
-    vk::VertexInputAttributeDescription posAttr{};
-    posAttr.location = 0;
-    posAttr.binding = 0;
-    posAttr.format = vk::Format::eR32G32Sfloat;
-    posAttr.offset = offsetof(Vertex, position);
-
-    std::vector<vk::VertexInputAttributeDescription> attributes;
-
-    attributes.push_back(posAttr);
-
-    for (uint32_t i = 0; i < 4; ++i) {
-        vk::VertexInputAttributeDescription instAttr{};
-        instAttr.location = 1 + i;
-        instAttr.binding = 1;
-        instAttr.format = vk::Format::eR32G32B32A32Sfloat;
-        instAttr.offset = offsetof(InstanceData, model) + sizeof(glm::vec4) * i;
-        attributes.push_back(instAttr);
-    }
-
-    vk::PipelineVertexInputStateCreateInfo vertexInput{};
-    vertexInput.vertexBindingDescriptionCount = static_cast<uint32_t>(bindings.size());
-    vertexInput.pVertexBindingDescriptions = bindings.data();
-    vertexInput.vertexAttributeDescriptionCount = static_cast<uint32_t>(attributes.size());
-    vertexInput.pVertexAttributeDescriptions = attributes.data();
-
-    PipelineConfig pipelineConfig{};
-    pipelineConfig.vertexInputStateCreateInfo = vertexInput;
-
-    return pipelineConfig;
 }
