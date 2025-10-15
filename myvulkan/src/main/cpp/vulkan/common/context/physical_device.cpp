daea39fd65607e68d053100d921423a9fcb3c6ee
k-kento <k.kento.918.124@gmail.com>
2025-10-16 08:21:07 +0900

    physical_device に findMemoryType メソッドを追加

--- myvulkan/src/main/cpp/vulkan/common/context/physical_device.cpp
+++ myvulkan/src/main/cpp/vulkan/common/context/physical_device.cpp
@@ -4,25 +4,37 @@
 #include "log.h"
 #include <stdexcept>
 
-PhysicalDevice::PhysicalDevice(const vk::Instance& instance) {
+PhysicalDevice::PhysicalDevice(const vk::Instance &instance) {
     if (!pickPhysicalDevice(instance)) {
         throw std::runtime_error("Failed to pick physical device");
     }
     LOGI("Physical device created.");
 }
 
-// Vulkan インスタンスに接続されている GPU の中から選択
-bool PhysicalDevice::pickPhysicalDevice(const vk::Instance& instance) {
-    std::vector<vk::PhysicalDevice> gpus = instance.enumeratePhysicalDevices();
-    if (gpus.empty()) {
+// 指定したプロパティを満たすメモリタイプのインデックスを返す
+uint32_t PhysicalDevice::findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties) {
+    vk::PhysicalDeviceMemoryProperties memProperties = mPhysicalDevice.getMemoryProperties();
+
+    for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+        if ((typeFilter & (1 << i)) && (memProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+            return i;
+        }
+    }
+    throw std::runtime_error("Failed to find suitable memory type.");
+}
+
+// 接続されているデバイスの中からグラフィックスコマンドを処理できるものを探す
+bool PhysicalDevice::pickPhysicalDevice(const vk::Instance &instance) {
+    std::vector<vk::PhysicalDevice> devices = instance.enumeratePhysicalDevices();
+    if (devices.empty()) {
         LOGE("PhysicalDevice: No GPU found");
         return false;
     }
 
-    for (auto gpu : gpus) {
-        uint32_t qIndex = findGraphicsQueueFamily(gpu);
+    for (auto device: devices) {
+        uint32_t qIndex = findGraphicsQueueFamily(device);
         if (qIndex != UINT32_MAX) {
-            mPhysicalDevice = gpu;
+            mPhysicalDevice = device;
             mQueueFamilyIndex = qIndex;
             LOGI("PhysicalDevice: GPU selected with graphics queue family %u", qIndex);
             return true;
--- myvulkan/src/main/cpp/vulkan/common/context/physical_device.h
+++ myvulkan/src/main/cpp/vulkan/common/context/physical_device.h
@@ -5,17 +5,19 @@
 class PhysicalDevice {
 
 public:
-    PhysicalDevice(const vk::Instance& instance) noexcept(false);
+    PhysicalDevice(const vk::Instance &instance) noexcept(false);
 
     [[nodiscard]] uint32_t getQueueFamilyIndex() const { return mQueueFamilyIndex; }
 
     [[nodiscard]] vk::PhysicalDevice getPhysicalDevice() const { return mPhysicalDevice; }
 
+    uint32_t findMemoryType(uint32_t typeFilter, vk::MemoryPropertyFlags properties);
+
 private:
     vk::PhysicalDevice mPhysicalDevice;
     uint32_t mQueueFamilyIndex = UINT32_MAX;
 
-    bool pickPhysicalDevice(const vk::Instance& instance);
+    bool pickPhysicalDevice(const vk::Instance &instance);
 
     static uint32_t findGraphicsQueueFamily(vk::PhysicalDevice device);
 
