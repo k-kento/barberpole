ad96a0e04089114b97d9f4869fdf485254a2854f
k-kento <k.kento.918.124@gmail.com>
2025-10-16 21:41:37 +0900

    UBO バッファを更新

--- myvulkan/src/main/cpp/vulkan/common/memory/ubo_buffer.hpp
+++ myvulkan/src/main/cpp/vulkan/common/memory/ubo_buffer.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
+#include "vulkan_context.h"
 #include "device_buffer.h"
 
 template<typename T>
 class UboBuffer {
 public:
-    explicit UboBuffer(vk::Device &device, vk::PhysicalDevice physicalDevice)
-            : mDevice(device) {
+    explicit UboBuffer(VulkanContext &context) {
         mBuffer = std::make_unique<DeviceBuffer>(
-                device,
-                physicalDevice,
+                context,
                 sizeof(T),
                 vk::BufferUsageFlagBits::eUniformBuffer,
                 vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent
@@ -20,11 +19,14 @@ public:
         mBuffer->copyFrom(&data, sizeof(T));
     }
 
-    vk::Buffer getBuffer() const { return mBuffer->getBuffer(); }
+    vk::Buffer getBuffer() const {
+        return mBuffer->getBuffer();
+    }
 
-    vk::DeviceSize getSize() const { return sizeof(T); }
+    vk::DeviceSize getSize() const {
+        return sizeof(T);
+    }
 
 private:
-    vk::Device &mDevice;
     std::unique_ptr<DeviceBuffer> mBuffer;
 };
