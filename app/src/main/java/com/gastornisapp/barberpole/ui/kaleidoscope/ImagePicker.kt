ebe79d5bd33dc0998d7304bb6dc2722e39e9ddec
k-kento <k.kento.918.124@gmail.com>
2025-10-22 08:15:42 +0900

    ImagePicker を修正

--- app/src/main/java/com/gastornisapp/barberpole/ui/kaleidoscope/ImagePicker.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/kaleidoscope/ImagePicker.kt
@@ -1,7 +1,5 @@
 package com.gastornisapp.barberpole.ui.kaleidoscope
 
-import android.graphics.BitmapFactory
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
@@ -14,17 +12,9 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import java.io.IOException
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun DrawablePicker(
@@ -47,29 +37,11 @@ fun DrawablePicker(
                 border = if (isSelected) BorderStroke(4.dp, MaterialTheme.colorScheme.primary) else null,
                 shape = MaterialTheme.shapes.medium,
             ) {
-                AssetImage(imageItem.assetPath)
+                Image(
+                    painter = rememberAsyncImagePainter(imageItem.assetPath),
+                    contentDescription = null
+                )
             }
         }
-    }
-}
-
-@Composable
-fun AssetImage(assetName: String) {
-    val context = LocalContext.current
-    var imageBitmap by remember { mutableStateOf<ImageBitmap?>(null) }
-
-    LaunchedEffect(assetName) {
-        try {
-            context.assets.open("images/$assetName").use { inputStream ->
-                val bitmap = BitmapFactory.decodeStream(inputStream)
-                imageBitmap = bitmap.asImageBitmap()
-            }
-        } catch (e: IOException) {
-            Log.e("DrawablePicker", "image error", e)
-        }
-    }
-
-    imageBitmap?.let {
-        Image(bitmap = it, contentDescription = null)
     }
 }
