6d14fce0f3655b7cb5c6280240c63064ff5dce95
k-kento <k.kento.918.124@gmail.com>
2025-10-23 16:05:25 +0900

    Image Picker 表示を調整

--- app/src/main/java/com/gastornisapp/barberpole/ui/kaleidoscope/ImagePicker.kt
+++ app/src/main/java/com/gastornisapp/barberpole/ui/kaleidoscope/ImagePicker.kt
@@ -3,7 +3,9 @@ package com.gastornisapp.barberpole.ui.kaleidoscope
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -12,7 +14,9 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.rememberAsyncImagePainter
 
@@ -37,10 +41,16 @@ fun DrawablePicker(
                 border = if (isSelected) BorderStroke(4.dp, MaterialTheme.colorScheme.primary) else null,
                 shape = MaterialTheme.shapes.medium,
             ) {
-                Image(
-                    painter = rememberAsyncImagePainter(imageItem.assetPath),
-                    contentDescription = null
-                )
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Image(
+                        painter = rememberAsyncImagePainter(imageItem.assetPath),
+                        contentDescription = null,
+                        contentScale = ContentScale.Fit
+                    )
+                }
             }
         }
     }
