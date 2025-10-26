97b4b03dff597a713b9c44f3315bde41603e5b03
k-kento <k.kento.918.124@gmail.com>
2025-10-26 13:14:05 +0900

    アプリのホームアイコン画像を変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.gastornisapp.barberpole"
-        minSdk = 29
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "0.0.1"
