72761ac3dcf65a0a85d42343151c334297ca0c7d
k-kento <k.kento.918.124@gmail.com>
2025-11-10 13:12:51 +0900

    compute shader 変更

--- myvulkan/src/main/cpp/vulkan/features/drawing/src/compute_buffer.hpp
+++ myvulkan/src/main/cpp/vulkan/features/drawing/src/compute_buffer.hpp
@@ -4,6 +4,7 @@
 #include "vulkan_context.h"
 #include "input_vertex.hpp"
 #include "log.h"
+#include "renderer_constants.hpp"
 
 class ComputeBuffer {
 
@@ -12,11 +13,8 @@ public:
     ComputeBuffer(VulkanContext &vkContext) {
         auto device = vkContext.getDevice();
 
-        uint32_t MAX_INPUT_POINTS = 1000; // TODO
-        const uint32_t MAX_OUTPUT_VERTICES = (MAX_INPUT_POINTS - 1) * 6; // TODO
-
         vk::BufferCreateInfo bufferInfo{};
-        bufferInfo.size = sizeof(InputVertex) * MAX_OUTPUT_VERTICES;
+        bufferInfo.size = sizeof(InputVertex) * MAX_COMPUTE_OUTPUT_VERTICES;
         bufferInfo.usage = vk::BufferUsageFlagBits::eStorageBuffer | vk::BufferUsageFlagBits::eVertexBuffer;
         bufferInfo.sharingMode = vk::SharingMode::eExclusive;
 
@@ -39,20 +37,6 @@ public:
 
     [[nodiscard]] vk::Buffer getBuffer() const {
         return mSharedBuffer.get();
-    }
-
-    void debugDump(VulkanContext &vkContext, uint32_t count) const {
-        vkContext.getDevice().waitIdle();
-        auto device = vkContext.getDevice();
-        void* mapped = device.mapMemory(mSharedMemory.get(), 0, VK_WHOLE_SIZE);
-
-        auto* vertices = reinterpret_cast<const InputVertex*>(mapped);
-
-        for (uint32_t i = 0; i < count; ++i) {
-            LOGD("output %f, %f", vertices[i].position.x, vertices[i].position.y);
-        }
-
-        device.unmapMemory(mSharedMemory.get());
     }
 
 private:
---
+++ myvulkan/src/main/cpp/vulkan/features/drawing/src/compute_params.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include <cstdint>
+
+struct ComputeParams {
+    uint32_t startIndex;
+    uint32_t pointCount;
+};
